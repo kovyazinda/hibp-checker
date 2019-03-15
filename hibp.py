eae33eaaea87e979000d608be5f6e9a68939dab8
kovyazinda <kovyazinda@gmail.com>
2019-03-15 14:15:20 +0600

    Create hibp.py

---
+++ hibp.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import csv
+import sys
+import time
+
+url = 'https://haveibeenpwned.com/api/v2/breachedaccount/'
+
+headers = requests.utils.default_headers()
+headers.update({
+        'User-Agent': 'HIBP-checker-python-v.0.1',
+#    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'
+    })
+
+#print(data_json)
+
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+
+if sys.argv[1] and sys.argv[2]:
+ infile=open(inputfile,"r")
+ outfile=open(outputfile,"w+")
+ csvrecord = csv.writer(outfile, delimiter=';',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+ headerarray=["Email","Title","BreachDate","Description","IsSensitive"]
+ csvrecord.writerow(headerarray)
+
+ for string in infile:
+
+#print (data_json[0]['Title'])
+#print (data_json[0]['BreachDate'])
+#print (data_json[0]['Name'])
+#print (data_json[0]['IsSensitive'])
+
+  target=url+string.rstrip()
+  try:
+   response = requests.get(target,headers=headers)
+   print (target)
+   print (response)
+   print (response.text)
+
+   data_json = json.loads(response.text)
+   i=0
+   for result in data_json:
+    print (i)
+    resulttitle=data_json[i]['Title']
+    resultbreachdate=data_json[i]['BreachDate']
+    resultdescription=data_json[i]['Description']
+    resultsensitivity=data_json[i]['IsSensitive']
+    valuearr=[string,resulttitle,resultbreachdate,resultdescription.encode('ascii', 'ignore'),resultsensitivity]
+    csvrecord.writerow(valuearr)
+    time.sleep(5)
+    i += 1
+
+
+  except:
+   print ("Query Error")
+
